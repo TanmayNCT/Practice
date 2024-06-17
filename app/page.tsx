c1b6a3abe3cc1a6251eeca9f0937ee9d5a6f5b57
TanmayNCT <tanmay.mantur@ncircletech.com>
2024-06-18 01:06:21 +0530

    Added approach page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -13,11 +17,14 @@ export default function Home() {
     >
       <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+          navItems={navItems}
         />
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients/>
+        <Experience/>
+        <Approach/>
       </div>
     </main>
   );
